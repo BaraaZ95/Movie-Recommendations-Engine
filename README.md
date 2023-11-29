d81056d7873008c4b9a1028c196a8ec388fe33d8
BaraaZ95 <archangle.650@gmail.com>
2023-11-29 16:53:12 +0400

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,85 @@
 # MLOPs Recommendation Engine
-End to end MLOPs recommendation engine from cleaning the data, training and logging the model using Weights and Biases, writing the data to Postgresql, making an API using FastApi and Designing a frontend using Streamlit
+## Overview
 
+This project is a web-based recommendation engine application that utilizes the cosine similarity algorithm. The application is built with a backend powered by FastAPI, a frontend developed with Streamlit, and containerized using Docker Compose. The data for the recommendation system is logged to Weights and Biases, and the similarity matrix is stored in a PostgreSQL database.
+
+
+## Prerequisites
+
+Make sure you have the following prerequisites installed:
+
+- Docker
+- Docker Compose
+- Python 3.10
+- pip (Python package installer)
+- Postgreql
+
+## Installation
+
+Clone the repository and install the Python dependencies. 
+
+Configuration
+
+Set up your configuration by modifying the .env file:
+
+ini
+
+.env environment variables
+```
+POSTGRES_USER=your_postgres_user
+POSTGRES_PASSWORD=your_postgres_password
+POSTGRES_DB=your_postgres_database
+POSTGRES_HOST = your_postgres_host
+WANDB_API_KEY=your_weights_and_biases_api_key
+```
+Usage
+
+The web app cannot be instantly launched since I am have a ver large csv file of the similarity matrix (10000 x 10000).
+
+To replicate the project, you will need to retrain the similarity matrix.
+
+Just go to `train_model.py` and run it. It will automatically load the dataset, clean the data and write it to postgresql. Then you are good to go.
+
+After obtaining the similarity matrix:
+
+`bash
+docker-compose up --build`
+
+Visit http://localhost:8501 in your web browser to access the recommendation engine web app.
+
+Data Flow
+
+    Cosine Similarity Algorithm:
+        The recommendation system algorithm used is cosine similarity.
+        Data is logged to Weights and Biases for analysis.
+
+    PostgreSQL Database:
+        The data for the similarity matrix and the movies dataset are stored in a PostgreSQL database.
+
+    FastAPI Backend:
+        The backend, powered by FastAPI, retrieves data from the PostgreSQL database.
+        Endpoints are available to query the similarity matrix and the movie imdb_ids for getting the posters for the frontend.
+
+    Streamlit Frontend:
+        The frontend is developed using Streamlit.
+        Users interact with the recommendation system through the Streamlit app.
+
+Logging
+
+Data is logged to Weights and Biases for monitoring and analysis. Use the Weights and Biases dashboard to gain insights into the recommendation system's performance and behavior.
+Backend
+
+The backend is built with FastAPI, providing a RESTful API to interact with the recommendation system. Key endpoints include:
+
+    /api/similarity-matrix: Retrieve the similarity matrix data.
+
+Frontend
+
+The frontend is developed using Streamlit, offering a user-friendly interface for users to interact with the recommendation system.
+Docker Compose
+
+The entire application is containerized using Docker Compose. The docker-compose.yml file defines the services and their configurations.
+
+Testing
+
+Separte tests are created: one for the API, and one for the database connection. Just head to movie_api/test and run `pytest`.
--- movie_model/ml_model/predict.py
+++ movie_model/ml_model/predict.py
@@ -1,65 +1,68 @@
-import sys
-from pathlib import Path
-from typing import Any
-
-# import json
-file = Path(__file__).resolve()
-parent, root = file.parent, file.parents[2]
-ROOT = str(root)
-sys.path.append(ROOT)
-from movie_model import __version__
-import pandas as pd
-
-# from skops.io import load
-from data.data_validation import validate_inputs, MovieNotFoundError
-import logging
-
-_logger = logging.getLogger(__name__)
-
-
-def predict_movie(
-    title: str,
-    similarity_weight: float = 0.7,
-    top_n: int = 10,
-) -> Any:
-    validated_input, errors = validate_inputs(input=title)
-    if not errors:
-        _logger.info(f"Making predictions with model version: {__version__} ")
-        # Load the movies dataset and the similarity matrix
-        filename_data = ROOT + "/movie_model/data/clean/final_df.parquet"
-        filename_sim = ROOT + "/movie_model/data/clean/cos_sim.parquet"
-        data = pd.read_parquet(
-            filename_data,
-            columns=["score", "id", "original_title"],
-        ).reset_index()
-        # Check if the movie is available before loading the similarity matrix
-        if validated_input not in data["original_title"].values:
-            raise MovieNotFoundError(input)
-        else:
-            cos_sim = pd.read_parquet(filename_sim)
-            index_movie = data[data["original_title"] == "Toy Story"].index[0]
-            similarity = cos_sim.values[index_movie].T
-            sim_df_ = pd.DataFrame(similarity, columns=["similarity"])
-            final_df = pd.concat([data, sim_df_], axis=1)
-            # The number can be tinkered with
-            final_df["final_score"] = (
-                final_df["score"] * (1 - similarity_weight)
-                + final_df["similarity"] * similarity_weight
-            )
-            final_df_sorted = final_df.sort_values(
-                by="final_score", ascending=False
-            ).head(top_n)
-            final_df_sorted = final_df_sorted.drop(
-                columns=["score", "similarity", "final_score"]
-            ).reset_index()
-            final_df_sorted.index = final_df_sorted.index + 1  # Start the index from 1
-
-            results = final_df_sorted.rename(
-                columns={"original_title": "similar_movies"}
-            )[["similar_movies", "id"]].to_dict()
-            results.update({"version": __version__, "errors": errors})
-            # results = json.dumps(results)
-            return results
-
-
-# print(predict_movie(title="toy Story"))
+# File not needed since Postgresql will be used to retrive the data
+#
+# import sys
+# from pathlib import Path
+# from typing import Any
+#
+## import json
+# file = Path(__file__).resolve()
+# parent, root = file.parent, file.parents[2]
+# ROOT = str(root)
+# sys.path.append(ROOT)
+# from movie_model import __version__
+# import pandas as pd
+#
+## from skops.io import load
+# from data.data_validation import validate_inputs, MovieNotFoundError
+# import logging
+#
+# _logger = logging.getLogger(__name__)
+#
+#
+# def predict_movie(
+#    title: str,
+#    similarity_weight: float = 0.7,
+#    top_n: int = 10,
+# ) -> Any:
+#    validated_input, errors = validate_inputs(input=title)
+#    if not errors:
+#        _logger.info(f"Making predictions with model version: {__version__} ")
+#        # Load the movies dataset and the similarity matrix
+#        filename_data = ROOT + "/movie_model/data/clean/final_df.parquet"
+#        filename_sim = ROOT + "/movie_model/data/clean/cos_sim.parquet"
+#        data = pd.read_parquet(
+#            filename_data,
+#            columns=["score", "id", "original_title"],
+#        ).reset_index()
+#        # Check if the movie is available before loading the similarity matrix
+#        if validated_input not in data["original_title"].values:
+#            raise MovieNotFoundError(input)
+#        else:
+#            cos_sim = pd.read_parquet(filename_sim)
+#            index_movie = data[data["original_title"] == "Toy Story"].index[0]
+#            similarity = cos_sim.values[index_movie].T
+#            sim_df_ = pd.DataFrame(similarity, columns=["similarity"])
+#            final_df = pd.concat([data, sim_df_], axis=1)
+#            # The number can be tinkered with
+#            final_df["final_score"] = (
+#                final_df["score"] * (1 - similarity_weight)
+#                + final_df["similarity"] * similarity_weight
+#            )
+#            final_df_sorted = final_df.sort_values(
+#                by="final_score", ascending=False
+#            ).head(top_n)
+#            final_df_sorted = final_df_sorted.drop(
+#                columns=["score", "similarity", "final_score"]
+#            ).reset_index()
+#            final_df_sorted.index = final_df_sorted.index + 1  # Start the index from 1
+#
+#            results = final_df_sorted.rename(
+#                columns={"original_title": "similar_movies"}
+#            )[["similar_movies", "id"]].to_dict()
+#            results.update({"version": __version__, "errors": errors})
+#            # results = json.dumps(results)
+#            return results
+#
+#
+## print(predict_movie(title="toy Story"))
+#
